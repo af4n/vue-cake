903f5d52751cbe2d1482df839dfa3ea28a43c4d1
af4n <aleksey.afanaskin@gmail.com>
2020-12-21 13:28:13 +0200

    init project

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+new Vue({
+  el: '.sample',
+  data: {
+    layers: [],
+    layersTypes: {
+      biscuit: {
+        price1sm: 50,
+        label: 'Biscuit',
+      },
+      beze: {
+        price1sm: 40,
+        label: 'Beze',
+      },
+      curd: {
+        price1sm: 60,
+        label: 'Curd'
+      }
+    },
+    defaultLayerType: 'biscuit',
+    defaultHeight: 4
+  },
+  computed: {
+    price() {
+      let sum = 0
+
+      this.layers.forEach((layer) => {
+        sum += layer.height * this.layersTypes[layer.type].price1sm
+      })
+
+      return sum
+    },
+    hasLayers() {
+      return this.layers.length > 0
+    }
+  },
+  methods: {
+    addLayer() {
+      this.layers.push({
+        type: this.defaultLayerType,
+        height: this.defaultHeight
+      })
+    },
+    changeHeight(i, dh) {
+      this.layers[i].height += dh;
+    },
+    deleteLayer(i) {
+      this.layers.splice(i, 1)
+    }
+  }
+})
